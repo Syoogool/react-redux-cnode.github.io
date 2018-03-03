70311947f5d1168f3b1262a53244171b05c48265
Syoogool <1944210109@qq.com>
2018-03-03 14:52:52 +0800

    feat: add a Create component

--- src/containers/Create/Create.jsx
+++ src/containers/Create/Create.jsx
@@ -1,15 +1,130 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import { Form, TextArea, Select, Button } from 'semantic-ui-react'
+import styled from 'styled-components'
+import Notification from 'rc-notification'
+import 'rc-notification/assets/index.css'
 import Navbar from '../../components/Navbar'
+import HeaderNav from '../../components/HeaderNav'
+import NoLogin from '../../components//NoLogin'
 
-class Create extends React.Component {
+
+let notification = null
+Notification.newInstance({}, (n) => notification = n)
+
+// 外边距合并问题
+const Content = styled.div`
+  padding-top: 50px;
+`
+const options = [
+  { key: 'share', text: '分享', value: 'share' },
+  { key: 'ask', text: '问答', value: 'ask' },
+  { key: 'job', text: '招聘', value: 'job'},
+  { key: 'dev', text: '测试', value: 'dev'}
+]
+
+class CreateForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.resetForm = this.resetForm.bind(this)
+    //表单数据初始化
+    this.state = {
+      title: '',
+      tab: '',
+      content: ''
+    }
+  }
+  
+  // 受控表单
+  handleChange = (e, { name, value }) => this.setState({ [name]: value})
+
+  // 重置表单
+  resetForm () {
+    this.setState({
+      title: '',
+      tab: '',
+      content: ''
+    })
+  }
+
+  simpleFn () {
+    notification.notice({
+      content: <span>创建主题成功</span>,
+      duration: 2,
+      onClose () {
+        console.log('simple close')
+      },
+      style: {
+        border: '1px solid green'
+      }
+    })
+  }
+  
+  // 表单提交处理事件
+  handleSubmit = () => {
+    const { tab, title, content } = this.state
+    const url = 'https://cnodejs.org/api/v1/topics'
+    this.setState({ title, tab, content })
+
+    axios.post(url,{
+      title,
+      tab,
+      content,
+      accesstoken: this.props.token
+    })
+    .then(res => {
+      // 重置表单
+      this.resetForm()
+      // 创建成功提示消息
+      this.simpleFn()
+      console.log(res)
+    })
+    .then(error => {
+      console.log(error)
+    })
+  }
+
   render () {
+    const { title, tab, content } = this.state
     return (
-      <div>
-        <h1>create page</h1>
-        <Navbar />
-      </div>
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Field control={Select} name='tab' value={tab} onChange={this.handleChange} options={options} placeholder='请选择测试类型' />
+        <Form.Group widths='equal'>
+          <Form.Input fluid name='title' value={title} onChange={this.handleChange} placeholder='标题字数不少于10字' />
+        </Form.Group>
+        <Form.Field control={TextArea} name='content' value={content} onChange={this.handleChange} placeholder='内容字数不少于30字' />
+        <Form.Field control={Button}>提交</Form.Field>
+        <pre>{JSON.stringify({ tab, title, content }, null, 2)}</pre>
+      </Form>
     )
   }
 }
 
-export default Create
+class Create extends React.Component {
+  render () {
+    const { auth } = this.props
+    console.log(this.props)
+    
+    return (
+      <React.Fragment>
+        <HeaderNav title='新建主题' />
+        <Content> 
+          { auth.isAuthenticated ?
+            <CreateForm token={auth.token} /> : <NoLogin />
+          }
+        </Content>
+        <Navbar />
+      </React.Fragment> 
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  const { auth } = state
+  return {
+    auth
+  }
+}
+
+export default connect(mapStateToProps)(Create)
