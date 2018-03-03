8ccadf8f0921b9f5c961e493262a6f8ac9c54454
Syoogool <1944210109@qq.com>
2018-03-03 15:25:02 +0800

    feat: create a notice component

---
+++ src/components/Notifice.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Notification from 'rc-notification'
+import 'rc-notification/assets/index.css'
+
+let notification = null
+Notification.newInstance({}, (n) => notification = n)
+
+const Notifice = props => {
+  notification.notice({
+    content: <span>{ props.content }</span>,
+    duration: 2,
+    onClose () {
+      console.log('simple close')
+    },
+    style: {
+      border: '1px solid green'
+    }
+  })
+}
+
+export default Notifice
