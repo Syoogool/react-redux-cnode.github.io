8aaa54227b4754790e9c1ff85c0b39e5e1826b9c
Syoogool <1944210109@qq.com>
2018-03-03 21:40:44 +0800

    feat(component): add 用户登陆的使用axios的错误处理，并设置了错误提示信息

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,24 @@
+import React from 'react'
 import axios from 'axios'
 import { push } from 'react-router-redux'
+import Notification from 'rc-notification'
+import 'rc-notification/assets/index.css'
+
+let notification = null
+Notification.newInstance({}, (n) => notification = n)
+
+const notifice = function (val) {
+  notification.notice({
+    content: <span>{ val }</span>,
+    duration: 2,
+    style: {
+      transform: 'translateX(-50%)',
+      borderRadius: '5px',
+      background: 'rgba(0, 0, 0, .8)',
+      color: '#fff'
+    }
+  })
+}
 
 export const questAuth = token => ({
   type: 'QUEST_AUTH',
@@ -23,11 +42,17 @@ export const auth = (token, fromURL) => dispatch => {
   dispatch(questAuth(token))
   axios.post(`https://cnodejs.org/api/v1/accesstoken?accesstoken=${token}`)
   .then(res => {
+    notifice('登陆成功')
     dispatch(authSucess(token, res.data))
     // 登陆成功后导航到之前的路由页面,默认才是导航到首页
     dispatch(push(willToURL))
   })
-  .catch(err => {
-    dispatch(authFail(token, err))
+  .catch(error => {
+    if (error.response) {
+      notifice(error.response.data.error_msg)
+    } else {
+      console.log('Error', error.message)
+    }
+    dispatch(authFail())
   })
 }
