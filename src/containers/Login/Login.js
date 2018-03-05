2a5af44320f42f37f872bd797da1caac2b0f54ae
Syoogool <1944210109@qq.com>
2018-03-05 22:39:25 +0800

    feat: 添加表单为受控组件

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,35 +1,58 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import classNames from 'classnames'
 import { auth } from '../../actions/auth'
 import { Form, Button } from 'semantic-ui-react'
 
 class LoginContainer extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      length: 0
+    }
     this.submit = this.submit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
   submit () {
     const { dispatch } = this.props
-    const { value } = this.refs.token
-    const state = this.props.history.location.state
-    // 上一页面导航路由
 
+    // String 类型refs(如下)不推荐使用 推荐使用回调函数式
+    // const { value } = this.refs.token
+    const { value } = this.input
+    const state = this.props.history.location.state
+
+    // 上一页面导航路由
     const fromURL = state ? state.from.pathname : '/'
 
     dispatch(auth(value, fromURL))
   }
 
+  handleChange () {
+    this.setState({
+      length: this.input.value.trim().length
+    })
+  }
+
   render () {
+    const submitClass = classNames({
+      'disabled': this.state.length < 1
+    })
     return (
       <div>
         <h2>登陆页面</h2>
-        {/* 非受控组件 ref获取表单值 */}
+        {/* 受控组件 ref获取表单值 */}
         <Form>
           <Form.Field>
             <label>Token</label>
-            <input type='text' ref='token' defaultValue='' placeholder='token' />
+            <input
+              type='text'
+              ref={(input) => { this.input = input }}
+              defaultValue=''
+              placeholder='token'
+              onChange={this.handleChange}
+            />
           </Form.Field>
-          <Button type='submit' onClick={this.submit}>登陆</Button>
+          <Button type='submit' className={submitClass} onClick={this.submit}>登陆</Button>
         </Form>
       </div>
     )
