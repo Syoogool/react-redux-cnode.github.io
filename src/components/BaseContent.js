b95581086d793d48259a3aa701a4856eda87098e
Syoogool <1944210109@qq.com>
2018-03-06 01:40:57 +0800

    refactor: 分离布局组件BaseContent

---
+++ src/components/BaseContent.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+/**
+ * [BaseContent 基本的内容布局组件]
+ * @type {[type]}
+ */
+const BaseContent = styled.div`
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 50px 20px;
+  text-align: left;
+`
+
+export default BaseContent
